39b7121803749b648e0cf3caa88d9253e466d346
Marco Spedaletti <asimov@mclink.it>
2021-01-12 21:43:18 +0100

    Moved to Apache 2.0 license.

--- img2tile.c
+++ img2tile.c
@@ -1,7 +1,31 @@
-/****************************************************************************
- * img2tile - Utility to convert images into (a set of                *
- *                                                                          *
- * Copyright (c) 2020 by Marco Spedaletti. Licensed under CC-BY-NC-SA       *
+/*****************************************************************************
+ * IMG2TILE - Utility to convert images into (a set of) tile(s)              *
+ *****************************************************************************
+ * Copyright 2020 Marco Spedaletti (asimov@mclink.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *----------------------------------------------------------------------------
+ * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+ * (la "Licenza"); è proibito usare questo file se non in conformità alla
+ * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+ * il software distribuito nei termini della Licenza è distribuito
+ * "COSÌ COM'È", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+ * implicite. Consultare la Licenza per il testo specifico che regola le
+ * autorizzazioni e le limitazioni previste dalla medesima.
  ****************************************************************************/
 
 // This directive is used to deactivate the safety warning for the use 
@@ -149,12 +173,9 @@ void usage_and_exit(int _level, int _argc, char* _argv[]) {
 
     printf("\n");
     printf("\n");
-    printf("------------------------------------------------------------\n");
     printf("img2tile - Utility to convert images into (a set of) tile(s)\n");
-    printf("------------------------------------------------------------\n");
     printf("Copyright(c) 2020 by Marco Spedaletti.\n");
-    printf("Licensed under CC-BY-NC-SA\n\n");
-
+    printf("Licensed under Apache 2.0 License\n\n");
     printf("Usage: %s [options]", _argv[0]);
     printf("\n");
     printf("options:\n");
